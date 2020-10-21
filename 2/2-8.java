4aa2760c42b05c809a378aa84a8769f4ddbf7542
Wonjun Lee <5laughter.luna@gmail.com>
2020-10-22 02:48:39 +0900

    2-8

---
+++ 2/2-8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class ToPound {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double kg = input.nextDouble();
+        System.out.println(kg);
+        System.out.println(kg*2.2);
+    }
+}
