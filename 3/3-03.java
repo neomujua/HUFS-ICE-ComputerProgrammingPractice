8ceac9800b9e4db966b215dcb5b6e8837a4ff734
Wonjun Lee <5laughter.luna@gmail.com>
2020-10-22 03:14:52 +0900

    3-03

---
+++ 3/3-03.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double kg = input.nextDouble();
+        System.out.printf("%.3f", kg*0.453592);
+    }
+}
