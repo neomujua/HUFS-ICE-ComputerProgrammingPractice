b43c01463eaaed9cfd2222dfc3c36ba143894d73
Wonjun Lee <5laughter.luna@gmail.com>
2020-10-22 03:12:49 +0900

    3-02

---
+++ 3/3-02.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        a = (int)input.nextDouble();
+        b = (int)input.nextDouble();
+        System.out.println(a+b);
+        System.out.println((a+b)/2);
+    }
+}
