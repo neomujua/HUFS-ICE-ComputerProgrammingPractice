eef63faaa740eeaed9ebe5f5a39aa1d83dc5be51
Wonjun Lee <5laughter.luna@gmail.com>
2020-10-22 02:54:41 +0900

    2-9

---
+++ 2/2-9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ToDigit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.printf("%o %d %x", a, a, a);
+    }
+}
