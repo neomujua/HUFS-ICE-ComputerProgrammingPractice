496414e1aae300eabba2e94394e2550ce2d10c1a
Wonjun Lee <5laughter.luna@gmail.com>
2020-12-21 05:42:46 +0900

    7-07 사용하지 않는 클래스 import 삭제

--- chapter7/7-07.java
+++ chapter7/7-07.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.HashSet;
-import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
