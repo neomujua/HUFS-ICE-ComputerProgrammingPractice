2f7107ccf313a6748eea2455f1b86201c73ea47c
Wonjun Lee <5laughter.luna@gmail.com>
2020-12-21 07:21:52 +0900

    Create 8-05.java

---
+++ chapter8/8-05.java
@@ -0,0 +1,30 @@
+class PrimeNumber02 extends Thread {
+    int n;
+
+    public PrimeNumber02(int n, String name) {
+        setName(name);
+        this.n = n;
+    }
+
+    public void run() {
+        System.out.println(getName()+":");
+        int j = 0;
+        for(int i=2; i<=this.n; i++) {
+            for(j=2; j<i; j++) {
+                if(i%j==0) {
+                    break;
+                }
+            }
+            if(i==j) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeNumber02 pn = new PrimeNumber01(20);
+        pn.start();
+    }
+}
